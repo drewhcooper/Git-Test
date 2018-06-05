c205d852f2597c8eeb982b661566c8622c5507c4
Andrew Cooper <acooper@consultwithcase.com>
2018-06-04 22:06:31 -0400

    String: Hellow world

--- File.c
+++ File.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+<<<<<<< HEAD
   int x = 1;
   int z = 2;
   for (int x = 0; x < 7; x++)
@@ -8,4 +9,7 @@ int main()
     z++;
   }
   printf("%d\n", x);
+=======
+  printf("Hello World\n");
+>>>>>>> String: Hellow world
 }
